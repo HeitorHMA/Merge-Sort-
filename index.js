80b7f55ed7883a437f854370a171dabe3f27aec0
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-04-26 04:45:38 -0300

    create merge sort function

---
+++ index.js
@@ -0,0 +1,12 @@
+function mergeSort(array){
+    console.log(array.lenght)
+    if(array.lenght < 2){
+        return array
+    }
+    else{
+        const mid = Math.floor(array.lenght)
+        console.log(array.lenght)
+    }
+}
+const arr = [2,2,2,2]
+mergeSort(arr)
